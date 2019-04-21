7a569f432820672db0e21a4b6f86813107399fd1
Pedro Frohmut <pedrofrohmut@gmail.com>
2019-04-21 11:03:14 -0300

    Empty DB Msg

--- src/components/MealsList.jsx
+++ src/components/MealsList.jsx
@@ -5,6 +5,7 @@ class MealsList extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      emptyDBMsg: "",
       totalCalories: 0,
       meals: []
     }
@@ -15,40 +16,33 @@ class MealsList extends Component {
       .then(meals => {
         const totalCalories = meals.reduce((acc, curr) => acc + curr.calories, 0)
         this.setState({ meals, totalCalories })
+        if (meals.length === 0) {
+          this.setState({ emptyDBMsg: "Its empty! No meals to list from database." })
+        }
       })
       .catch(err => console.log(err))
-  }
-
-  getTotalCalories = meals => {
-    return 0
   }
 
   render() {
     return (
       <>
-        {/* Total Calories */}
-        <h5 className="total-calories-header">
-          {this.state.meals.length > 0 ? (
-            <>
-              Total Calories: <span className="total-calories">{this.state.totalCalories}</span>
-            </>
-          ) : (
-            <>No meals found at the database</>
-          )}
-        </h5>
+        {this.state.emptyDBMsg && <p className="empty-db-msg">{this.state.emptyDBMsg}</p>}
 
-        {/* Meals List */}
         {this.state.meals.length > 0 && (
-          <ul className="meals-list">
-            {this.state.meals.map((meal, i) => (
-              <li key={i}>
-                <strong>{meal.name}</strong> <em>{meal.calories} Calories</em>
-                <a href="#" className="edit-meal" onClick={e => this.props.onSetEditingState(meal.id, e)}>
-                  Edit <i className="fa fa-pencil" />
-                </a>
-              </li>
-            ))}
-          </ul>
+          <>
+            <h5 className="total-calories-header">Total Calories: {this.state.totalCalories}</h5>
+
+            <ul className="meals-list">
+              {this.state.meals.map((meal, i) => (
+                <li key={i}>
+                  <strong>{meal.name}</strong> <em>{meal.calories} Calories</em>
+                  <a href="#" className="edit-meal" onClick={e => this.props.onSetEditingState(meal.id, e)}>
+                    Edit <i className="fa fa-pencil" />
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </>
         )}
       </>
     )
